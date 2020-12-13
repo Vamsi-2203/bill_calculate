75e66af6f5cb1aea280f7df912719228871533e5
Vamsidhar Reddy <67990409+Vamsi-2203@users.noreply.github.com>
2020-12-13 22:30:04 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,77 @@
+import math
+from datetime import datetime
+
+name = input('enter your name:')
+
+
+def bill():
+    a = True
+    total_price = 0
+    list_item_price = []
+    while a:
+        print('''
+            1. items
+            2. choose item
+            3. exit
+        ''')
+
+        choice = int(input('enter your choice:'))
+        choices = [1, 2, 3]
+        if choice in choices:
+            d = {'dal': 23, 'oil': 35, 'floor': 33, 'meat': 500, 'fish': 200, 'paste': 20, 'ice': 60}
+            if choice == 1:
+                c = 1
+                for i, j in d.items():
+                    print('\t', c, '.', i, '$', j)
+                    c += 1
+            if choice == 2:
+                x = True
+                while x:
+                    print("press 'x' to  see main menu.")
+                    item = input('enter item:')
+                    if item in d.keys():
+                        qnt = input('enter Quantity:')
+                        if qnt.isdigit():
+
+                            qnt = int(qnt)
+                            price = float(d[item] * qnt)
+                            list_item_price.append((item, qnt, price))
+                            total_price += price
+                        else:
+                            print('Invalid Input Quantity..')
+                    elif item == 'x':
+                        x = False
+
+                    else:
+                        print('item not present.')
+
+            if choice == 3:
+                 a = False
+        else:
+            print('Invalid Input..Please Enter a valid input.')
+    return total_price, 'Thank you, Please Visit again.', list_item_price
+
+
+total, msg, item_price = bill()
+if total != 0:
+    print('\n')
+    print('''
+              vc market
+    ''')
+    print('Customer Name:', name, '\t', 'Date:', datetime.now())
+    print(25 * '**')
+
+
+    for j in item_price:
+        print('Item:', j[0], '\tQuntity:', j[1], ' Price:', j[2])
+
+    gst = total * 0.2
+    gst = math.ceil(gst)
+    print(25 * '**')
+    print('GST: ', float(gst))
+    print('Total payble amount: Rs.', float(gst + total))
+    print(25 * '**')
+    print('🙏🙏🙏 ', msg, ' 🙏🙏🙏')
+else:
+    print("Hey, You weren't Brought anything...Please Buy something you want.")
+    bill()
